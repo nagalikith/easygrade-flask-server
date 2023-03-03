eb0b8db2236f0846c121e69d2bdd165b869646de
Naga Likith <likiths082@gmail.com>
2023-03-03 14:07:17 +0530

    Moved Init, Added User Model

---
+++ src/db.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, flash, click, current_app, g
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50, nullable=False))
