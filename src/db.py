390ff68a0d10c5b3c823b9a8408d76d167941007
Naga Likith <likiths082@gmail.com>
2023-03-23 11:41:54 +0530

    Added models to db

--- src/db.py
+++ src/db.py
@@ -11,5 +11,45 @@ db.init_app(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50, nullable=False))
+    password = db.Column(db.String(50), nullable=False)
+
+
+class Assignment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    max_submissions = db.Column(db.Integer, nullable=False)
+    max_score = db.Column(db.Integer, nullable=False)
+
+class Submission(db.Model):
+    assignment_id = db.Column(db.Integer, db.ForeignKey('assignment.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    submission_id = db.Column(db.String(50), primary_key=True)
+    submission_time_client = db.Column(db.DateTime, nullable=False)
+    submission_time_server = db.Column(db.DateTime, nullable=False)
+    summary = db.Column(db.String(50), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    s3_url = db.Column(db.String(50), nullable=False)
+
+class Submission_Aggregate(db.Model):
+    assignment_id = db.Column(db.Integer, db.ForeignKey('assignment.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    submission_id = db.Column(db.String(50), db.ForeignKey('submission.submission_id'), nullable=False)
+    summary = db.Column(db.String(50), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+participation = db.Table(
+    'participation',
+    SQLAlchemy.Column('assignment_id', SQLAlchemy.Integer, SQLAlchemy.ForeignKey('assignment.id'), primary_key=True),
+    SQLAlchemy.Column('student_id', SQLAlchemy.Integer, SQLAlchemy.ForeignKey('user.id'), primary_key=True),
+    SQLAlchemy.Column('num_of_submissions', SQLAlchemy.Integer, nullable=False))
+
+
+
+
+                         
